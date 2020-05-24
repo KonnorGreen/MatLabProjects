73352adbee11898d5f55f3526192c3710bfb8bde
Konnor Green <jmldgreen6@gmail.com>
2020-05-23 20:12:36 -0500

    Update RAW2PNG.m

--- RAW2PNG.m
+++ RAW2PNG.m
@@ -1,7 +1,9 @@
+%Base Diretory or can change to filepath, replace .CR2 with extension
 f=dir('*.CR2');
 fil={f.name};  
 for k=1:numel(fil)
   file=fil{k};
+  %replace.png to desired output extension
   new_file=strrep(file,'.CR2','.png');
   im=imread(file);
   imwrite(im,new_file);
